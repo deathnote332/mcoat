c6edfaec5e2b95955159d43ff992f4c85258f434
John Paul Inhog <aexilsxd@gmail.com>
2017-09-28 11:21:41 +0800

    update

--- public/themes/default/layouts/default.blade.php
+++ public/themes/default/layouts/default.blade.php
@@ -7,7 +7,7 @@
         <meta name="keywords" content="{!! Theme::get('keywords') !!}">
         <meta name="description" content="{!! Theme::get('description') !!}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=0">
-        <meta name="csrf_token" content="{{ csrf_token() }}">
+
         {!! Theme::asset()->styles() !!}
         {!! Theme::asset()->scripts() !!}
     </head>
