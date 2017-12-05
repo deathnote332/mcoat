4647fa81447544ce33a3cb78b69479a8713e773a
John Paul Inhog <aexilsxd@gmail.com>
2017-12-05 11:04:28 +0800

    update

--- public/themes/default/partials/sidebar.blade.php
+++ public/themes/default/partials/sidebar.blade.php
@@ -182,16 +182,8 @@
 
     <!-- Page Content -->
     <div id="page-wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-
-                    {!! Theme::content() !!}
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </div>
+        {!! Theme::content() !!}
+        
         <!-- /.container-fluid -->
     </div>
     <!-- /#page-wrapper -->
